7ef25f333a948e73ff9cb8c28510df534e928b3e
brettsteph <cloudengineer777@gmail.com>
2023-03-11 14:47:33 -0500

    Change variable

--- route53.tf
+++ route53.tf
@@ -13,7 +13,7 @@ data "aws_acm_certificate" "amazon-issued-cert" {
 
 resource "aws_route53_record" "a_record" {
   zone_id = data.aws_route53_zone.hosted_zone.zone_id
-  name    = var.sub-domain
+  name    = var.sub_domain
   type    = "A"
   # ttl     = "300"
   #for ALB
